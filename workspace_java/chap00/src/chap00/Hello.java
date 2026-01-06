2e9c10f6e974fd1d9429b9c22306f8812de98f8a
lys010327@naver.com <>
2026-01-06 09:43:11 +0900

    1월 5일 수업

---
+++ workspace_java/chap00/src/chap00/Hello.java
@@ -0,0 +1,28 @@
+package chap00;
+
+public class Hello {
+
+	// 한 줄 주석
+	/* 범위 주석 */
+	/* 여러줄
+	  주석
+	 */
+	/**
+	 * javadoc 주석
+	 * @param args
+	 */
+	/*
+	 * 주석과 출력이 가장 중요하다!!
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world"); //출력문
+	}
+
+	
+	// first name
+	// first_name // snake case
+	// first-name // 캐밥 case
+	// firstName  // 카멜camel case
+}

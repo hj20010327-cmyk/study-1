7390f435a77337397845f47d9933182520e15765
용상\lys01 <lys010327@naver.com>
2026-01-10 23:40:21 +0900

    집에서 진짜 열심히 풀었다...

--- workspace_java/chap04/src/chap04/ForExam_03.java
+++ workspace_java/chap04/src/chap04/ForExam_03.java
@@ -135,28 +135,28 @@ public class ForExam_03 {
 		
 		// 문제 2
 		// 1~ 100까지 홀수의 합과 개수
-		sum = 0;
-		int count = 0;
-		
-		
-		for (num = 1; num <= 100; num++ ) {
-			if (num%2 == 1) {
-				sum = sum + num;
-				count++;
-			}
-		}
-		System.out.print(sum + " ");
-		System.out.println(count);
+//		sum = 0;
+//		int count = 0;
+//		
+//		
+//		for (num = 1; num <= 100; num++ ) {
+//			if (num%2 == 1) {
+//				sum = sum + num;
+//				count++;
+//			}
+//		}
+//		System.out.print(sum + " ");
+//		System.out.println(count);
 		// 문제 3
 		// 1~입력 받은 수 까지 더하기
 
-		Scanner scan = new Scanner(System.in);
-		int rsp1 = scan.nextInt();
-		sum = 0;
-		for (num =1; num <= rsp1; num++) {
-			sum = sum + num;
-		}
-		System.out.println(sum);
+//		Scanner scan = new Scanner(System.in);
+//		int rsp1 = scan.nextInt();
+//		sum = 0;
+//		for (num =1; num <= rsp1; num++) {
+//			sum = sum + num;
+//		}
+//		System.out.println(sum);
 		
 		// ----- 조금 난이도 있는 ---------
 		// 문제 4
@@ -182,7 +182,9 @@ public class ForExam_03 {
 		// 문제 1
 		// 구구단 모든 단 출력
 		int t;
+		t = 2;
 		int p;
+		p = 1;
 		for (t = 2; t <= 9; t++) {
 			System.out.println("   " + t + "단");
 			for (p = 1; p <= 9; p++) {
@@ -191,25 +193,162 @@ public class ForExam_03 {
 			}
 		}
 		
-		// 문제 2
-		// 구구단 단마다 출력
+		// 문제2
+        // 구구단 단마다 옆으로 출력
+        // 2x1=2 2x2=4...
 		
-		// 문제 3
-		// 구구단 3단씩 옆으로 출력
+		for ( t=2; t <= 9; t++) {
+			System.out.println(" " + t + "단");
+			for (p = 1; p <= 9; p++) {
+				int m = t * p;
+				if (p == 9) {
+				System.out.println(t + " x " + p + " = " + m);
+				} else {
+					System.out.print(t + " x " + p + " = " + m + " ");
+				}
+			}
+		}
+        
+        // 문제3
+        // 구구단 3단씩 옆으로 출력
+        // 2x1=2 3x1=3 4x1=4
+        // 2x2=4 3x2=6 4x2=8
+        // 2x3=6...
+        t = 2;
+        p = 1;
+		for (p = 1; p <= 9; p++) {
+			for (t = 2; t<=9; t++) {
+				int o = t * p;
+				if (t == 9) {
+				System.out.println(t + "x" + p + " = " + o);
+				} else {
+					System.out.print(t + "x" + p + " = " + o + " ");
+				}
+			}
+			
+		}
+        // 문제4
+        // 주사위 2개를 굴려서
+        // 나올 수 있는 모든 조합 출력
+        // [1,1] [1,2] [6,6]
 		
-		// 문제 4
-		// 주사위 2개를 굴려서 
-		// 나올 수 있는 모든 조합 출력
+		int dice1;
+		int dice2;
+		for (dice1 = 1; dice1 <= 6; dice1++) {
+			for (dice2 =1; dice2 <=6; dice2++) {
+				if (dice2 == 6) {
+				System.out.println( "[" + dice1 + "," +  dice2 + "]");
+				} else {
+					System.out.print( "[" + dice1 + "," +  dice2 + "]" + " ");					
+				}
+			}
+		}
 		
-		// 문제 5
-		// 주사위 2개를 굴려서
-		// 합 별로 출력
+        
+        // 문제5
+        // 주사위 2개를 굴려서
+        // 합 별로 출력
+        // 합2 : [1,1]
+        // 합3 : [1,2] [2,1]
 		
-		// 문제 6
-		// 주사위 2개를 굴려서 나올 조합에서
-		// 중복 없이 출력
-		// 예 : [1,2] [2.1] 중복이라서 [1,2]
-		// 중복이라는 개수
+//		합h = dice1+ dice2
+//		h 2 3 4 5 6 ... 2부터 하나씩 증가
+//		주사위 두개 합이 h면 되는데
+//		if (h == dice1 + dice2) {
+//			System.out.println(합 h: [dice1, dice2]);		
+//		}
+		
+		int h;
+		dice1 = 1;
+		dice2 = 1;
+		String str3;
+		str3 = "";
+		for (h = 2; h <= 12; h++) {
+			str3 = "합" + h + ": ";
+		for (dice1 = 1; dice1 <= 6; dice1++) {
+			for (dice2 =1; dice2 <=6; dice2++) {
+				if (h == dice1 + dice2) {					
+					str3 += "[" + dice1 + "," + dice2 + "]";
+					} 
+				}			
+			}
+			System.out.println(str3);	// h의 반복문 안에서 주사위 반복문이 다 끝나서 출력해야 되므로 
+										// 주사위 반복문 다음에 출력해야함
+		}
+		System.out.println();
+	
+		
+        // 문제6
+        // 주사위 2개를 굴려서 나올 조합에서
+        // 중복 없이 출력
+        // 예: [1,2] [2,1] 중복이라서 [1,2]
+		
+		// 주사위 2개 굴리는데
+		// 중복이 없어야됨
+		// 1,1 1,2 2,1
+		// dice2가 dice1의 값에서 시작하면 없어지나?
+		// dice1이 1이면 dice2 1부터 시작 1,1 ~ 1,6 출력
+		// dice1이 2면 dice2 2부터 시작 2,2 ~ 2,6 출력 2,1 출력 안됨
+		// dice1이 3이면 dice2 3부터 시작 3,3 ~ 3,6    3,1 3,2 출력 안됨 // 미친 유레카
+		
+		for (dice1 = 1; dice1 <= 6; dice1++) {
+			for (dice2 = dice1; dice2 <=6; dice2++) {
+				if (dice2 == 6) {
+					System.out.println( "[" + dice1 + "," +  dice2 + "]");
+				} else {
+						System.out.print( "[" + dice1 + "," +  dice2 + "]" + " ");					
+				}
+			}			
+		}
+		
+		// 피라미드 만들기
+//		---*---  1           4줄일땐  3 1   5줄일땐  4 1
+//		--***--  3 * =2*line-1        2 2			 3 2
+//		-*****-  5                    1 3			 2 3
+//	    *******  7					  0 4			 1 4
+//											         0 5 						
+//	         										얘네 둘 합 = 줄 개수	minus + line = n	
+//		이걸 어떻게 만들까					4줄이라면       첫번쨰줄일때		3  +  1 = 4	 minus는 3개
+//		4줄이 반복  															 2  +  2 = 4   minus가 n-line일때 멈춰야됨
+//		- 가 반복 되는데 그다음 줄에는 *가 늘어남 (2개씩) 2n-1 
+//		- 반복되고 * 쓰고 - 다시 반복 => 반복문 3개 써야됨	
+//		
+//		
+//		
+		int line;
+		int star;
+		int minus;
+		int n = 4;
+		star = 1;
+		for (line= 1; line<= n; line++) {
+			for (minus =1; minus <= n - line ; minus++ ) {
+				System.out.print("-");
+			}
+			for (star = 1; star <= 2*line -1; star++ ) {
+				System.out.print("*");
+			}
+			for (minus =1; minus <= n - line ; minus++ ) {
+				System.out.print("-");
+			}
+		System.out.println();
+		}
+		for (line= n-1; line>= 1; line--) {
+			for (minus =1; minus <= n - line ; minus++ ) {
+				System.out.print("-");
+			}
+			for (star = 1; star <= 2*line -1; star++ ) {
+				System.out.print("*");
+			}
+			for (minus =1; minus <= n - line ; minus++ ) {
+				System.out.print("-");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
 		
 		// 1~ 100까지 쓰는데 10의 배수마다 엔터치기
 //		int e;

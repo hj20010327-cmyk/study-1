6a49a294a0765415802de0175ad6b2c7204acbd4
lys010327@naver.com <>
2026-01-15 16:40:16 +0900

    1월 15일 수업

--- workspace_java/chap05/src/chap05/ArrayExam_02.java
+++ workspace_java/chap05/src/chap05/ArrayExam_02.java
@@ -1,5 +1,7 @@
 package chap05;
 
+import java.util.Scanner;
+
 public class ArrayExam_02 {
 
 	public static void main(String[] args) {
@@ -270,6 +272,311 @@ public class ArrayExam_02 {
 			}
 		}
 		System.out.println("가장 큰 수: " + result);
+		
+		
+		// 문제 7
+		// {3,4,7,5,1,4,6}
+		// 여기서 두번째 큰 수 찾기
+		System.out.println("-------문제7--------");
+		int two = 0;
+		for(i=0; i<big.length; i++) {
+			if(big[i]<result && big[i]>two) {
+				two = big[i];
+			}
+		}
+		System.out.println("두번째 큰 수: " + two);
+		// 문제 8
+		// 오른쪽으로 한칸 씩 밀기 
+		// (왼쪽은 0으로 채우기)
+		// 예:
+		// 1라운드: {3, 4, 7, 5, 1, 4, 6}
+        // 2라운드: {0, 3, 4, 7, 5, 1, 4}	// 6을 0으로 만들고 big[0]으로 보냄
+											// big[0]이 big[1]로 이동
+        // 3라운드: {0, 0, 3, 4, 7, 5, 1}
+		
+		// 1라운드 3,4,7,5,1,4,6
+		System.out.println("-------문제 8---------");
+		for(j=1; j<=8; j++) {
+			System.out.print(j+ "라운드: {");
+			for(i=0; i<big.length; i++) {
+				if(i == big.length-1) {
+					System.out.print(big[i]);
+				} else {
+					System.out.print(big[i]+ ", ");
+				}
+			}
+			System.out.println("}");
+			for(i=big.length-1; i>0; i--) {   //big[0] = 0 big[1] = 3
+				big[i] = big[i-1];
+			}
+			big[0] = 0;
+		}
+		
+		// 문제 9
+		// 오른쪽으로 한칸 씩 밀기
+		// 맨 끝 값은 맨 처음으로 보내기
+		// 예: 
+        // 1라운드: {3, 4, 7, 5, 1, 4, 6}
+        // 2라운드: {6, 3, 4, 7, 5, 1, 4}
+        // 3라운드: {4, 6, 3, 4, 7, 5, 1}
+		System.out.println("-------문제 9--------");
+		int[] big1 = {3,4,7,5,1,4,6};
+		int p;
+		p=0;
+		for(j=1; j<=8; j++) {
+			System.out.print(j+ "라운드: {");
+			for(i=0; i<big1.length; i++) {
+				if(i == big1.length-1) {
+					System.out.print(big1[i]);
+				} else {
+					System.out.print(big1[i]+ ", ");
+				}
+			}
+			System.out.println("}");
+			p = big1[big1.length-1];
+			for(i=big1.length-1; i>0; i--) {   //big[0] = 0 big[1] = 3
+				big1[i] = big1[i-1];
+			}
+			big1[0] = p;
+		}
+		
+		
+		// 문제 10
+		// 임시 비밀번호 8자리
+		// 10-1 : 숫자만
+		System.out.println("--------문제10--------");
+		int[] w = new int[8];
+//		System.out.println(g);
+		for(i=0; i<w.length; i++) {
+			double u = Math.random();
+			double u1 = u*10;
+			int g = (int)u1;
+			w[i] = g;
+		}
+		for(j=0; j<w.length; j++) {
+			System.out.print(w[j]);
+		} 
+		System.out.println();
+		// 10-2 : 소문자만
+		char[] w1 = new char[8];
+		
+		for(i=0; i<w1.length; i++) {
+			double o = Math.random();
+			double o1 = o*26;
+			int o12 = (int)o1+97;
+			char v = (char)o12;
+			w1[i]= v;
+			System.out.print(w1[i]);
+		}
+		System.out.println();
+		// 10-3 : 숫자2개 이상, 대/소문자 각 1개 이상
+//		double uu = Math.random();
+//		double uu1 = uu*5;			
+//		int gg = (int)uu1+2;
+		int[] ww = new int[6];
+		for(i=0; i<ww.length; i++) {
+			double u = Math.random();
+			double u1 = u*10;
+			int g = (int)u1;
+			ww[i] = g;
+		}
+		for(j=0; j<ww.length; j++) {
+			if(ww.length>=2) {
+				System.out.print(ww[j]);
+			}
+		}
+		
+//		double dd = Math.random();
+//		double dd1 = dd*5;
+//		int ii1 = (int)dd1+1;
+		char[] ch  = new char[1];
+		
+		for(i=0; i<ch.length; i++) {
+			double o = Math.random();
+			double o1 = o*26;
+			int o12 = (int)o1+97;
+			char v = (char)o12;
+			w1[i]= v;
+			System.out.print(w1[i]);
+		}
+		char[] bigch = new char[1];
+		for (i=0; i<bigch.length; i++) {
+			double oo = Math.random();
+			double oo1 =  oo*26;
+			int oo2 = (int)oo1+65;
+			char vv = (char)oo2;
+			bigch[i] = vv;
+			System.out.print(bigch[i]);
+		}
+		
+		System.out.println();
+		// 문제 11
+		// 자리가 10개 있는 소극장 예약 시스템
+		// 자리 번호는 1~10번까지 번호의 자리가 있다
+		// 메뉴 : "1.예약 2.모든 좌석 현황 3.잔여 좌석 0.종료"
+		// 조건1 : 예약이 가능하다면 "n번 자리 예약되었습니다"
+		// 조건2 : 예약이 되어있다면 "이미 예약 되어 있습니다"
+		System.out.println("--------문제 11----------");
+		int[] small = new int[10];
+		for(i=0; i<small.length; i++) {
+			small[i] = i+1;
+		}
+		int[] small1 = new int[10];
+		for(i=0; i<small1.length; i++) {
+			small1[i] = i+1;
+		}
+		
+		
+		int x = 0;
+		int x1 = 0;
+		do {
+			System.out.println("메뉴: 1.예약 2.모든 좌석 현황 3.잔여 좌석 0.종료");
+			Scanner scan = new Scanner(System.in);
+			x = scan.nextInt();
+			int reser = 0;
+			int seat = 0;
+			if(x==1) {
+				System.out.println("선택> " + x);
+				for(i=0; i<small1.length; i++) {
+				System.out.print("["+small1[i]+"] ");
+				}
+				System.out.println();
+				Scanner scan1 = new Scanner(System.in);
+				x1 = scan1.nextInt();
+				for(i=0; i<small.length; i++) {
+				if(x1==small1[i]) {
+					if(small[i]!= reser) {
+						System.out.print(x1 + "번 자리가 예약 되었습니다");
+						small[i] = reser;
+					} else {
+						System.out.print("이미 예약된 자리입니다");
+					}
+					}
+				}
+				System.out.println();
+			} else if (x==2) {
+				System.out.println("선택> " + x + "\n 숫자가 0인 자리는 예약이 된 자리입니다");
+				System.out.print("모든 좌석 현황: ");
+				for(i=0; i<small.length; i++) {
+				System.out.print("["+small[i]+"] ");
+				}
+				System.out.println();
+			} else if (x==3) {
+				System.out.print("잔여 좌석: ");
+				for(i=0; i<small.length; i++) {
+					if(small[i] != 0) {
+						System.out.print("["+small[i]+"] ");
+					}
+				}
+				System.out.println();
+			}
+		} while(x!=0);
+		System.out.println("종료합니다");
+		
+		// 문제 12
+		// 로또 번호 6개 배열에 저장
+		// 단, 중복 없이!
+		System.out.println("---------문제12----------");
+		int[] lotto = new int[6];
+		int result1 = lotto[0];
+		for(i=0; i<lotto.length; i++) {
+			double u = Math.random();
+			double u1 = u*1000000;
+			int g = (int)u1%46+1;
+//			lotto[i] = g;   // 이게 중복이 안되야돼
+//			for(j=0; j<lotto.length; j++) {
+//				if(lotto[j] == l)
+//			}
+			int l = 0;
+			boolean boo = true;
+			while(boo) {
+				if(g == lotto[l]) {
+					u = Math.random();
+					u1 = u*1000000;
+					g = (int)u1%46+1;
+//					System.out.print("걸림");
+				} else {
+					l++;
+					
+				}
+				if(l == lotto.length-1) break;
+			}
+			lotto[i] = g;
+			System.out.print(g + " ");
+		}
+		System.out.println();
+		
+		// 문제 13
+		// 주차장에 
+		// 0: 주차되어있음 
+		// 1: 비어있음
+		/*
+		 * {
+		 * 		{0, 0, 0, 0},	// 1층
+		 * 		{0, 0, 0, 1},	// 2층
+		 * 		{1, 0, 1, 0},
+		 * 		{1, 0, 1, 1},
+		 * 		{1, 1, 1, 1}
+		 * }
+		 */
+		// 13-1 : 2층에 주차된 차량 수 출력
+		System.out.println("---------문제13-1----------");
+		int[] f1 = {0, 0, 0, 0};
+		int[] f2 = {0, 0, 0, 1};
+		int[] f3 = {1, 0, 1, 0};
+		int[] f4 = {1, 0, 1, 1};
+		int[] f5 = {1, 1, 1, 1};
+		
+		int cnt1 = 0;
+		
+		
+		int[][] parking = {f1,f2,f3,f4,f5};
+		for(i=0; i<parking.length; i++) {
+			for(j=0; j<parking[i].length; j++) {
+				if(i==1 && parking[i][j] == 0) {
+					cnt1++;
+				}
+			}
+		}
+		System.out.println("2층에는 " + cnt1 + "대가 주차되어있습니다");
+		// 13-2 : 전체 남은 자리 수 출력
+		System.out.println("----------문제 13-2-----------");
+		cnt1 =0;
+		for(i=0; i<parking.length; i++) {
+			for(j=0; j<parking[i].length; j++) {
+				if(parking[i][j] == 1) {
+					cnt1++;
+				}
+			}
+		}
+		System.out.println("모든 층에 주차자리가 "+ cnt1 + "자리 남았습니다");
+		
+		//문제 13 랜덤으로 돌려보기
+		cnt1 = 0;
+		int[][] plot = new int[5][4];
+		for(i=0; i<plot.length; i++) {
+			for(j=0; j<plot[i].length; j++) {
+				double park1 = Math.random();
+				double park3 = park1*2;
+				plot[i][j] = (int)park3;
+			}
+		}
+		for(i=0; i<plot.length; i++) {
+			for(j=0; j<plot[i].length; j++) {
+				System.out.print("{" + plot[i][j] + "}");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for(i=0; i<plot.length; i++) {
+			for(j=0; j<plot[i].length; j++) {
+				if(plot[i][j] == 1) {
+					cnt1++;
+				}
+			}
+		}
+		System.out.println(cnt1);
+		
 		
 	}
 
